ae475045183c8ae83f2299a8ec61b1787e4b828d
UsagiiTsukino <willgotofactory@gmail.com>
2025-07-22 16:48:14 +0700

    change mongodb cloud

--- src/config/db/mongodb.ts
+++ src/config/db/mongodb.ts
@@ -4,7 +4,7 @@ import { config } from "dotenv";
 async function connect() {
   config();
   try {
-    await mongoose.connect(process.env.MONG0DB_URI || "");
+    await mongoose.connect(process.env.MONGODB_URI || "");
     console.log("Connect Successfully");
   } catch (error) {
     console.log("Connect Fallure");
